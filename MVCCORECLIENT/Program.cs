009f8bae5894caf5d4102f88873c2eb73048b14a
IhorPetr <vep2014@ukr.net>
2017-08-25 19:37:07 +0300

    Add project

---
+++ MVCCORECLIENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MVCCORECLIENT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "MVCCoreClient";
+            var host = new WebHostBuilder()
+                .UseKestrel().UseUrls("http://localhost:5002")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ MVCFRAMEWORKCLENT/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFRAMEWORKCLENT.Startup))]
+namespace MVCFRAMEWORKCLENT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            //{
+            //    AuthenticationType = "Cookies",
+            //    ExpireTimeSpan = TimeSpan.FromMinutes(10),
+            //    SlidingExpiration = true
+            //});
+
+            //JwtSecurityTokenHandler.InboundClaimTypeMap.Clear();
+
+            //app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            //{
+            //    AuthenticationType = "oidc",
+            //    SignInAsAuthenticationType = "Cookies",
+            //    Authority = "http://localhost:5000/",
+            //    RedirectUri = "http://localhost:5002/signin-oidc",
+            //    ClientId = "mvc",
+            //    ResponseType = "id_token",
+            //    Scope = "openid profile",
+            //    UseTokenLifetime = false
+            //});
+            //app.UseStageMarker(PipelineStage.Authenticate);
+        }
+    }
+}
